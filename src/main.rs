f12099e2aeca4fcd518accf25a153f10a90119ca
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-11-26 23:01:44 +0600

    more loop example added

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
     // Loop, Iteretor, Reapitetion
 
-    // while loop
+    // while
     let mut num = 5;
     while num < 10 {
         println!("nuumber = {}", num);
         num += 1;
     }
-    
+
+    // loop
     loop {
         if num == 12 {
             println!("Ek Dozon");
@@ -22,4 +23,34 @@ fn main() {
         println!("Holy sholy Mother Teresa");
         num += 1;
     }
+
+    // naming loop
+    let mut num = 0;
+    'counter: loop {
+        println!("Brazil: {}", num);
+        let mut decrease = 5;
+        'checking: loop {
+            println!("Arzentina: {}", decrease);
+            if decrease == 4 {
+                break 'checking;
+            }
+            if num == 2 {
+                break 'counter;
+            }
+            decrease -= 1; //decrease = decrease - 1
+        }
+        num += 1; // num = num + 1
+    }
+
+    // for loop
+    let vec: Vec<i8> = (0..10).collect();
+
+    for element in vec {
+        println!("{}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}", number)
+    }
+    println!("LIFTOFF!!!!");
 }
